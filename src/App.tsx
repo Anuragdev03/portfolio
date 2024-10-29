1b2cf2a70a3d5a76dea79ae79c30be91ade7dfda
anurag <anuragdev03@gmail.com>
2024-10-29 14:43:33 +0530

    fix darkmode issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import linkendin from "./assets/linkedin.png";
 import github from "./assets/github.png";
