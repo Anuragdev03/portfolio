362af53c0332d400dbc0e57957eb3cda171f17f5
anurag <anuragdev03@gmail.com>
2025-01-31 17:18:02 +0530

    disable dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (import.meta.env.MODE === 'production') {
+  disableReactDevTools();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
